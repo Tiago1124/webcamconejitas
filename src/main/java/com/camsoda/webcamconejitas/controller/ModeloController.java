16df1cd26d7bf73e16678d1d53c650cf1375aaa5
Daniel Pareja Londoño <dpareja1394@gmail.com>
2025-08-23 11:30:04 -0500

    Modelos Controller y Mapper

--- src/main/java/com/camsoda/webcamconejitas/controller/ModeloController.java
+++ src/main/java/com/camsoda/webcamconejitas/controller/ModeloController.java
@@ -1,4 +1,26 @@
 package com.camsoda.webcamconejitas.controller;
 
+import com.camsoda.webcamconejitas.dto.ModeloDTO;
+import com.camsoda.webcamconejitas.mapper.ModeloMapper;
+import com.camsoda.webcamconejitas.model.Modelo;
+import com.camsoda.webcamconejitas.repository.ModeloRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/modelo")
+@RestController
 public class ModeloController {
+    private final ModeloRepository modeloRepository;
+
+    @GetMapping("/all")
+    public List<ModeloDTO> listarModelos() {
+        List<Modelo> modelos = modeloRepository.findAll();
+        List<ModeloDTO> modelosDTO = ModeloMapper.listModeloToDTO(modelos);
+        return modelosDTO;
+    }
 }
--- src/main/java/com/camsoda/webcamconejitas/mapper/ModeloMapper.java
+++ src/main/java/com/camsoda/webcamconejitas/mapper/ModeloMapper.java
@@ -3,9 +3,9 @@ package com.camsoda.webcamconejitas.mapper;
 import com.camsoda.webcamconejitas.dto.ModeloDTO;
 import com.camsoda.webcamconejitas.model.Modelo;
 
+import java.util.List;
+
 public class ModeloMapper {
-
-
     public static ModeloDTO modelToDTO(Modelo modelo) {
         return ModeloDTO.builder()
                 .idModelo(modelo.getIdModelo())
@@ -14,6 +14,9 @@ public class ModeloMapper {
                 .build();
     }
 
+    public static List<ModeloDTO> listModeloToDTO(List<Modelo> modelos) {
+        return modelos.stream().map(ModeloMapper::modelToDTO).toList();
+    }
 
     public static Modelo dtoToModel(ModeloDTO modeloDTO) {
         return Modelo.builder()
