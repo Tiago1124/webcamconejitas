b32d5c7f0d8d7d00c67805ed8896c40342bf23ec
santimurg12 <santimurg12@hotmail.com>
2025-08-23 11:17:57 -0500

    DTO con ID, nombre artista y correo

--- src/main/java/com/camsoda/webcamconejitas/dto/ModeloDTO.java
+++ src/main/java/com/camsoda/webcamconejitas/dto/ModeloDTO.java
@@ -1,4 +1,40 @@
-package com.camsoda.webcamconejitas.dto;
-
 public class ModeloDTO {
+
+    private Integer idModelo;
+    private String nombreArtista;
+    private String correo;
+
+
+
+
+    public ModeloDTO(Integer idModelo, String nombreArtista, String correo) {
+        this.idModelo = idModelo;
+        this.nombreArtista = nombreArtista;
+        this.correo = correo;
+    }
+
+
+    public Integer getIdModelo() {
+        return idModelo;
+    }
+
+    public void setIdModelo(Integer idModelo) {
+        this.idModelo = idModelo;
+    }
+
+    public String getNombreArtista() {
+        return nombreArtista;
+    }
+
+    public void setNombreArtista(String nombreArtista) {
+        this.nombreArtista = nombreArtista;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 }
