f4a9dd575f432b6c23ba82cdb3ab892cd23e55b9
Daniel Pareja Londoño <dpareja1394@gmail.com>
2025-08-23 11:10:39 -0500

    Repository y Mapper

---
+++ src/main/java/com/camsoda/webcamconejitas/mapper/ModeloMapper.java
@@ -0,0 +1,10 @@
+package com.camsoda.webcamconejitas.mapper;
+
+import com.camsoda.webcamconejitas.dto.ModeloDTO;
+import com.camsoda.webcamconejitas.model.Modelo;
+
+public class ModeloMapper {
+    ModeloDTO modeloToModeloDTO(Modelo modelo) {
+        return null;
+    }
+}
--- src/main/java/com/camsoda/webcamconejitas/repository/ModeloRepository.java
+++ src/main/java/com/camsoda/webcamconejitas/repository/ModeloRepository.java
@@ -1,4 +1,9 @@
 package com.camsoda.webcamconejitas.repository;
 
-public interface ModeloRepository {
+import com.camsoda.webcamconejitas.model.Modelo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ModeloRepository extends JpaRepository<Modelo, Integer> {
 }
