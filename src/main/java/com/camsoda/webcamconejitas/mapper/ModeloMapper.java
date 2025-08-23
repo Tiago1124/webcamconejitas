5fcebdeb745aef696ed6bbe37f50e646d8fb1790
Selena Delgado <mayraselenadm@gmail.com>
2025-08-23 11:28:03 -0500

    Mapper Modelo

--- src/main/java/com/camsoda/webcamconejitas/mapper/ModeloMapper.java
+++ src/main/java/com/camsoda/webcamconejitas/mapper/ModeloMapper.java
@@ -4,7 +4,22 @@ import com.camsoda.webcamconejitas.dto.ModeloDTO;
 import com.camsoda.webcamconejitas.model.Modelo;
 
 public class ModeloMapper {
-    ModeloDTO modeloToModeloDTO(Modelo modelo) {
-        return null;
+
+
+    public static ModeloDTO modelToDTO(Modelo modelo) {
+        return ModeloDTO.builder()
+                .idModelo(modelo.getIdModelo())
+                .nombreArtista(modelo.getNombreArtista())
+                .correo(modelo.getCorreo())
+                .build();
+    }
+
+
+    public static Modelo dtoToModel(ModeloDTO modeloDTO) {
+        return Modelo.builder()
+                .idModelo(modeloDTO.getIdModelo())
+                .nombreArtista(modeloDTO.getNombreArtista())
+                .correo(modeloDTO.getCorreo())
+                .build();
     }
 }
